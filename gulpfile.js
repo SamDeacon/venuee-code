67511553a37f56ff60df67f9e90f89da2f825f19
Sam Deacon <deaconsam10@gmail.com>
2015-11-22 02:09:16 +0000

    added autoprefixer to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
   return gulp.src('sass/mystyle.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+    }))
     .pipe(gulp.dest('css'))
 });
 
